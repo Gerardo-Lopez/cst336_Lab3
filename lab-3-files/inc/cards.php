f8339bbd0594cfc82a05d7f794dd4b69fad79364
Gerardo Lopez <gerlopez@csumb.edu>
2018-09-26 18:40:41 +0000

    update 9

--- lab-3-files/inc/cards.php
+++ lab-3-files/inc/cards.php
@@ -2,90 +2,89 @@
 
 
 
-                $deck = array("cards" => array());
+                $deck = array(array());
                
-                $deck["cards"][] = array();
             
                 for($t = 1;$t <= 52;$t++) {
                     
+                    
                     if($t <= 13 ) {
-                        $deck["cards"][$t]["value"] = $t;
-                        $deck["cards"][$t]["suit"] = "clubs";
-                        $deck["cards"][$t]["img"] = "src='img/clubs/$t.png' title='$t of clubs' width ='70'";
+                        
+                        $deck [$t][0] = $t;
+                        $deck [$t][1] = "clubs";
+                        $deck [$t][2] = "src='img/clubs/$t.png' title='$t of clubs' width ='70'";
+                        
+                        array_push($deck, array());
                     }
                     else if($t > 13 && $t <= 26 ) {
                         $s = $t - 13;
-                        $deck["cards"][$t]["value"] = $s;
-                        $deck["cards"][$t]["suit"] = "diamonds";
-                        $deck["cards"][$t]["img"] = "src='img/diamonds/$s.png' title='$s of diamonds' width ='70'";
+                        
+                        $deck [$t][0] = $s;
+                        $deck [$t][1] = "diamonds";
+                        $deck [$t][2] = "src='img/diamonds/$s.png' title='$s of diamonds' width ='70'";
+                        
+                        array_push($deck, array());                        
+                        
                     }
                     else if($t > 26 && $t <= 39 ) {
                         $s = $t - 26;
-                        $deck["cards"][$t]["value"] = $s;
-                        $deck["cards"][$t]["suit"] = "hearts";
-                        $deck["cards"][$t]["img"] = "src='img/hearts/$s.png' title='$s of hearts' width ='70'";
+                        
+                        $deck [$t][0] = $s;
+                        $deck [$t][1] = "hearts";
+                        $deck [$t][2] = "src='img/hearts/$s.png' title='$s of hearts' width ='70'";
+                        
+                        array_push($deck, array());                        
+                        
                     }
                     else if($t > 39 && $t <= 52 ) {
                         $s = $t - 39;
-                        $deck["cards"][$t]["value"] = $s;
-                        $deck["cards"][$t]["suit"] = "spades";
-                        $deck["cards"][$t]["img"] = "src='img/spades/$s.png' title='$s of spades' width ='70'";
+
+                        $deck [$t][0] = $s;
+                        $deck [$t][1] = "spades";
+                        $deck [$t][2] = "src='img/spades/$s.png' title='$s of spades' width ='70'";
                         
                     }
                 }
                 
                 // function shuffle_deck($deck){
-                     shuffle($deck["cards"]);
-                     
-                foreach ($deck["cards"] as $cards){
-                echo $cards ["value"];
-                echo $cards ["suit"];
-                echo "<img ".$cards ["img"]." />";
+                     shuffle($deck);
                 
-                echo '<br>';
-            }
-            echo 'DONE';
+                
+
+                
+                
+                
+            //     foreach ($deck["cards"] as $cards){
+            //     echo $cards["value"];
+            //     echo $handDelt = array_shift($cards["value"]);
+            //     echo $cards ["suit"];
+            //     echo $handDelt = array_shift($cards["suits"]);
+            //     echo "<img ".$cards ["img"]." />";
+            //     echo $handDelt = array_shift($cards["img"]);
+                
+            //     echo '<br>';
+            // }
+            // echo 'DONE';
             
-            echo '<br>';
+            // echo '<br>';
             
                 // }
                 
                 // shuffle_deck($deck);
 
 
-            $score1 = 0;
-            $score2 = 0;
-            $score3 = 0;
-            $score4 = 0;
 
-            function deal_hands($player1, $deck){
-                for ($i = 1; $i < 10; $i++){
-                    // while ($score1 <= 36){
-                    foreach ($deck["cards"] as $cards){
-                        
-                        if ($score1 >= 36)
-                        break;
-                        
-                        $score1 += $cards["value"];
-                        echo "<img ".$cards ["img"]." />";
-                        echo $score1;
-                        $handDelt = array_shift($deck["cards"]); // not yet working
-                        echo '<br>';
-                    }
-                    
-                // } 
-                
-            }
-            }
-            deal_hands($player1, $deck);
-            echo 'DONE';
-            echo '<br>';
-            foreach ($deck["cards"] as $cards){
-                echo $cards ["value"];
-                echo $cards ["suit"];
-                echo "<img ".$cards ["img"]." />";
-                echo '<br>';
-            }
+            
+            
+            // deal_hands($player1, $deck["cards"]);
+            // echo 'DONE';
+            // echo '<br>';
+            // foreach ($deck["cards"] as $cards){
+            //     echo $cards ["value"];
+            //     echo $cards ["suit"];
+            //     echo "<img ".$cards ["img"]." />";
+            //     echo '<br>';
+            // }
             
     
 ?>
--- lab-3-files/index.php
+++ lab-3-files/index.php
@@ -22,10 +22,145 @@ include 'inc/players.php';
             // players($player1, $player2, $player3, $player4);
             // shuffle_deck($deck);
             
-            deal_hands($player1, $deck);
+            // deal_hands($player1, $deck);
             
-            players($player1, $player2, $player3, $player4);
-            echo 'Total execution time in seconds: ' . (microtime(true) - $time_start);
+            // players($player1, $player2, $player3, $player4);
+            // echo 'Total execution time in seconds: ' . (microtime(true) - $time_start);
+
+
+                for($card = 0; $card < 53; $card++){
+                    for ($trait = 0; $trait < 3; $trait++){
+                    
+                    if($trait == 2)
+                    echo "<img ".$deck[$card][$trait]." />";
+                    
+                    else
+                    echo $deck[$card][$trait];    
+                    }
+                
+                echo '<br>';    
+                }
+                
+                echo '<h3>Done</h3>';
+                
+            $score1 = 0;
+            $score2 = 0;
+            $score3 = 0;
+            $score4 = 0;
+
+            function deal_hand1($deck){
+                        for ($card = 0; $card <= 52; $card++){
+                        if($score1 <= 36){
+                            
+                            $score1 += $deck[$card][0];         // increments score
+                            echo $score1;
+                            echo "<img ".$deck[$card][2]." />"; // echo's picture
+                            $deltCard = array_shift($deck);
+                        }
+                        else
+                            break;
+                        } 
+                        echo '<br>';
+            }
+
+
+            function deal_hand2($deck){
+                        for ($card = 0; $card <= 52; $card++){
+                        if($score2 <= 36){
+                            
+                            $score2 += $deck[$card][0];         // increments score
+                            echo $score2;
+                            echo "<img ".$deck[$card][2]." />"; // echo's picture
+                            $deltCard = array_shift($deck);
+                        
+                        }
+                        else
+                            break;
+                        
+                        } 
+                        echo '<br>';
+            }
+
+            function deal_hand3($deck){
+                        for ($card = 0; $card <= 52; $card++){
+                        if($score3 <= 36){
+                            
+                            $score3 += $deck[$card][0];         // increments score
+                            echo $score3;
+                            echo "<img ".$deck[$card][2]." />"; // echo's picture
+                            $deltCard = array_shift($deck);
+                        }
+                        else 
+                            break;
+                        }
+                        echo '<br>';
+            }
+
+            function deal_hand4($deck){
+                        for ($card = 0; $card <= 52; $card++){
+                        if($score4 <= 36){
+                            
+                            $score4 += $deck[$card][0];         // increments score
+                            echo $score4;
+                            echo "<img ".$deck[$card][2]." />"; // echo's picture
+                            $deltCard = array_shift($deck);
+                        }
+                        else
+                            break;
+                        }
+                        echo '<br>';
+            }
+
+
+
+
+
+
+
+
+
+                        echo '<h3>Done</h3>';
+                        
+
+
+                    // foreach ($deck["cards"] as $cards){
+                        
+                    //     if ($score1 >= 36)
+                    //     break;
+                        
+                    //     $score1 += $cards["value"];
+                    //     echo "<img ".$cards ["img"]." />";
+                    //     echo $score1;
+                    //     $handDelt = array_shift($deck["cards"]); // not yet working
+                    //     echo '<br>';
+                    // }
+                    
+                // } 
+                
+            // }
+                 
+            
+            deal_hand1($deck);
+            deal_hand2($deck);
+            deal_hand3($deck);            
+            deal_hand4($deck);            
+            
+            
+                            for($card = 0; $card < 52; $card++){
+                    for ($trait = 0; $trait < 3; $trait++){
+                    
+                    if($trait == 2)
+                    echo "<img ".$deck[$card][$trait]." />";
+                    
+                    else
+                    echo $deck[$card][$trait];    
+                    }
+                
+                echo '<br>';    
+                }
+                
+                echo '<h3>Done</h3>';
+
 
             ?>
             
