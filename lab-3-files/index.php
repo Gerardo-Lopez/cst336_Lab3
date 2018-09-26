adee4e826de5679cc71c751e21a2bf95f3a02255
Shawn Deppe <sdeppe@csumb.edu>
2018-09-26 07:35:39 +0000

    update 8

--- lab-3-files/index.php
+++ lab-3-files/index.php
@@ -2,30 +2,39 @@
 
 // include 'inc/cards.php';
 include 'inc/players.php'
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <title> Cards </title>
-        <style>
-            /*@import url("css/styles.css");*/
-        </style>
+            <link href="css/style5.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
+        <h1 class="center">Silver Jack</h1>
+        <hr>
+        <br />
+        <br />
         <div id="main"> 
-            
             <?php
             $time_start = microtime(true); 
             // I AM EMPTY
             players($player1, $player2, $player3, $player4);
-            echo 'Total execution time in seconds: ' . (microtime(true) - $time_start);
             ?>
             
         </div>
-        
-        <form>
-            <input type="submit" value="Play Again!"/>
-        </form>
+        <br />
+        <br />
+        <hr>
+        <div id="main2">
+            <?php
+                echo 'Total execution time in seconds: ' . (microtime(true) - $time_start);
+            ?>
+            <form id= main3>
+                <input type="submit" value="Play Again!"/>
+            </form>
+        </div>
     </body>
 </html>
